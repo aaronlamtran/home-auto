5f21aabf401deece487bd0673d9794e49e1c893b
Aaron Tran <altran32@gmail.com>
2022-10-24 20:52:52 -0700

    add gitignore, add controllers

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from flask import Flask
 from flask import send_file
 from flask import Response
+from flask import request
 import json
 from fan import Fan
 
@@ -12,10 +13,20 @@ app = Flask(__name__)
 bedroom_fan = Fan()
 
 
-@app.route("/")
+@app.route("/status")
+def get_status():
+    return bedroom_fan.get_status_fan()
+
+@app.route("/status", methods=['POST'])
+def set_status():
+    req = request.json
+    return bedroom_fan.set_status(req)
+
+@app.route("/light")
 def hello_world():
-    bedroom_fan.toggle_light()
-    return json.dumps(bedroom_fan.toggle_light(), indent=4)
+    response = bedroom_fan.toggle_light()
+    print(response)
+    return json.dumps(response, indent=4)
 
 
 if __name__ == "__main__":
